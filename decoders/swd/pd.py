dragonmux/libsigrokdecode

swd: Refactored the unknown state handler out into its own function

decoders/swd/pd.py
@@ -162,14 +162,17 @@ class Decoder(srd.Decoder):
 			self.annotateBits(self.startSample, self.samplenum, [A.WRITE, [f'{target} WRITE', f'{target} WR', f'{target[0]}W']])
 		self.state = DecoderState.ackTurnaround
 
+	def handle_unknown(self, swclk: Bit, swdio: Bit):
+		# If we're waiting on a line reset, then look only for a rising edge with swdio high
+		if swclk == 1 and swdio == 1:
+			self.startSample = self.samplenum
+			self.bits = 1
+			self.state = Decoder.reset
+
 	def handle_swclk_edge(self, swclk: Bit, swdio: Bit):
 		match self.state:
 			case DecoderState.unknown:
-				# If we're waiting on a line reset, then look only for a rising edge with swdio high
-				if swclk == 1 and swdio == 1:
-					self.startSample = self.samplenum
-					self.bits = 1
-					self.state = Decoder.reset
+				self.handle_unknown(swclk, swdio)
 
 			case DecoderState.idle:
 				# If this is the rising edge of the clock, check to see if we are leaving idle

OpenTraceLab/OpenTraceDecode

decoders/swd/pd.py
@@ -162,14 +162,17 @@ class Decoder(otd.Decoder):
 			self.annotateBits(self.startSample, self.samplenum, [A.WRITE, [f'{target} WRITE', f'{target} WR', f'{target[0]}W']])
 		self.state = DecoderState.ackTurnaround
 
+	def handleUnknown(self, swclk: Bit, swdio: Bit):
+		# If we're waiting on a line reset, then look only for a rising edge with swdio high
+		if swclk == 1 and swdio == 1:
+			self.startSample = self.samplenum
+			self.bits = 1
+			self.state = Decoder.reset
+
 	def handleClkEdge(self, swclk: Bit, swdio: Bit):
 		match self.state:
 			case DecoderState.unknown:
-				# If we're waiting on a line reset, then look only for a rising edge with swdio high
-				if swclk == 1 and swdio == 1:
-					self.startSample = self.samplenum
-					self.bits = 1
-					self.state = Decoder.reset
+				self.handleUnknown(swclk, swdio)
 
 			case DecoderState.idle:
 				# If this is the rising edge of the clock, check to see if we are leaving idle