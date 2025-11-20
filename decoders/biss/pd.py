kamocat/libsigrokdecode

BISS: Started decoder

decoders/biss/pd.py
@@ -1,7 +1,7 @@
 import sigrokdecode as srd
 
 class Decoder(srd.Decoder):
-    api_version = 2
+    api_version = 3
     id = 'biss'
     name = 'BISS'
     longname = 'Bidirectional/Serial/Synchronous'
@@ -22,25 +22,28 @@ class Decoder(srd.Decoder):
             {'id':'crc','description':'CRC polynomial', 'default':0x43},
         )
     annotations = (
-            ('bit', 'data bit'),
-            ('ack', 'Slave acknowledgement'),
-            ('start', 'Start bit'),
-            ('cds', 'Control communication slave->master'),
-            ('cdm', 'Control communication master->slave'),
-            ('linedelay', 'Line Delay'),
-            ('processing', 'Slave processing time'),
-            ('timeout', 'Slave timeout'),
-            ('data', 'Data'),
-            ('crc', 'CRC'),
+            ('bit', 'data bit'), #0
+            ('ack', 'Slave acknowledgement'), #1
+            ('start', 'Start bit'), #2
+            ('cds', 'Control communication slave->master'), #3
+            ('cdm', 'Control communication master->slave'), #4
+            ('linedelay', 'Line Delay'), #5
+            ('processing', 'Slave processing time'), #6
+            ('timeout', 'Slave timeout'), #7
+            ('data', 'Data'), #8
+            ('crc', 'CRC'), #9
         )
     annotation_rows = (
             ('bits', 'Bits', (0,1,2,3,4)),
             ('times', 'Times', (5,6,7)),
             ('data', 'Data', (8,9)),
         )
+    binary = (
+            ('data', 'Data')
+        )
 
     def __init__(self, **kwargs):
-        self.state = 'FIND LATCH'
+        self.state = 'IDLE'
 
     def metadata(self, key, value):
         if key == srd.SRD_CONF_SAMPLERATE:
@@ -48,10 +51,44 @@ class Decoder(srd.Decoder):
 
     def start(self):
         self.out_ann = self.register(srd.OUTPUT_ANN)
+        self.out_clockrate = self.register(srd.OUTPUT_META,
+                meta=(int, 'Clockrate', 'Master clock rate'))
 
-    def decode(self, ss, es, data):
-        for self.samplenum, (ma, sl) in data:
-            #decode the samples
+    def decode(self):
+        while True:
+            self.wait({0:'r'})
+            self.wait({0:'r'}) #Wait for the second rising edge
+            self.linedelay = self.samplenum
+            self.wait({1:'f'}) #Wait for ACK
+            self.ack = self.samplenum
+            self.ld = self.ack - self.linedelay
+            self.put(self.linedelay, self,ack, self.out_ann, [5, ['LineDelay','LD','L']], self.ld/self.samplerate)
+            self.clk_count = 0
+            while True:
+                self.clk_count += 1
+                self.wait({1:'r'}, {0:'e'}) #Count clock edges while waiting for start bit
+                if self.matched[0]:
+                    break
+            self.sb = self.samplenum
+            self.clk_half_period = (self.sb-self.ack)/self.clk_count
+            self.cp = int(self.clk_half_period)
+            self.put(self.sb, self.ack, self.out_clockrate, self.samplerate/self.clk_half_period/2)
+            self.put(self.ack, self,sb, self.out_ann, [1, ['ACK','A']])
+            self.put(self.ack, self,sb, self.out_ann, [6, ['Processing','Pt']])
+            while True:
+                #FIXME: Use the line delay
+                ma, sl = self.wait({0:f}, {'skip':3*self.cp+1})
+                if self.matched[1]:
+                    break
+                self.put(self.samplenum-self.cp, self.samplenum+self.cp, self.out_ann, [0, 'Bit', 'B'])
+            self.timeout = self.samplenum - 2*self.cp
+            self.wait({0:'h', 1:'h'})
+            self.idle = self.samplenum
+            self.put(self.timeout, self.idle, self.out_ann, [7, 'Timeout','T'])
+              
+              
+              
+              
 
 
 

OpenTraceLab/OpenTraceDecode

decoders/biss/pd.py
@@ -1,7 +1,7 @@
 import opentracedecode as otd
 
 class Decoder(otd.Decoder):
-    api_version = 2
+    api_version = 3
     id = 'biss'
     name = 'BISS'
     longname = 'Bidirectional/Serial/Synchronous'
@@ -22,25 +22,28 @@ class Decoder(otd.Decoder):
             {'id':'crc','description':'CRC polynomial', 'default':0x43},
         )
     annotations = (
-            ('bit', 'data bit'),
-            ('ack', 'Slave acknowledgement'),
-            ('start', 'Start bit'),
-            ('cds', 'Control communication slave->master'),
-            ('cdm', 'Control communication master->slave'),
-            ('linedelay', 'Line Delay'),
-            ('processing', 'Slave processing time'),
-            ('timeout', 'Slave timeout'),
-            ('data', 'Data'),
-            ('crc', 'CRC'),
+            ('bit', 'data bit'), #0
+            ('ack', 'Slave acknowledgement'), #1
+            ('start', 'Start bit'), #2
+            ('cds', 'Control communication slave->master'), #3
+            ('cdm', 'Control communication master->slave'), #4
+            ('linedelay', 'Line Delay'), #5
+            ('processing', 'Slave processing time'), #6
+            ('timeout', 'Slave timeout'), #7
+            ('data', 'Data'), #8
+            ('crc', 'CRC'), #9
         )
     annotation_rows = (
             ('bits', 'Bits', (0,1,2,3,4)),
             ('times', 'Times', (5,6,7)),
             ('data', 'Data', (8,9)),
         )
+    binary = (
+            ('data', 'Data')
+        )
 
     def __init__(self, **kwargs):
-        self.state = 'FIND LATCH'
+        self.state = 'IDLE'
 
     def metadata(self, key, value):
         if key == otd.SRD_CONF_SAMPLERATE:
@@ -48,10 +51,44 @@ class Decoder(otd.Decoder):
 
     def start(self):
         self.out_ann = self.register(otd.OUTPUT_ANN)
+        self.out_clockrate = self.register(otd.OUTPUT_META,
+                meta=(int, 'Clockrate', 'Master clock rate'))
 
-    def decode(self, ss, es, data):
-        for self.samplenum, (ma, sl) in data:
-            #decode the samples
+    def decode(self):
+        while True:
+            self.wait({0:'r'})
+            self.wait({0:'r'}) #Wait for the second rising edge
+            self.linedelay = self.samplenum
+            self.wait({1:'f'}) #Wait for ACK
+            self.ack = self.samplenum
+            self.ld = self.ack - self.linedelay
+            self.put(self.linedelay, self,ack, self.out_ann, [5, ['LineDelay','LD','L']], self.ld/self.samplerate)
+            self.clk_count = 0
+            while True:
+                self.clk_count += 1
+                self.wait({1:'r'}, {0:'e'}) #Count clock edges while waiting for start bit
+                if self.matched[0]:
+                    break
+            self.sb = self.samplenum
+            self.clk_half_period = (self.sb-self.ack)/self.clk_count
+            self.cp = int(self.clk_half_period)
+            self.put(self.sb, self.ack, self.out_clockrate, self.samplerate/self.clk_half_period/2)
+            self.put(self.ack, self,sb, self.out_ann, [1, ['ACK','A']])
+            self.put(self.ack, self,sb, self.out_ann, [6, ['Processing','Pt']])
+            while True:
+                #FIXME: Use the line delay
+                ma, sl = self.wait({0:f}, {'skip':3*self.cp+1})
+                if self.matched[1]:
+                    break
+                self.put(self.samplenum-self.cp, self.samplenum+self.cp, self.out_ann, [0, 'Bit', 'B'])
+            self.timeout = self.samplenum - 2*self.cp
+            self.wait({0:'h', 1:'h'})
+            self.idle = self.samplenum
+            self.put(self.timeout, self.idle, self.out_ann, [7, 'Timeout','T'])
+              
+              
+              
+              
 
 
 